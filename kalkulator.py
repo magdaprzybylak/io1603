6dd1ef4b499e47ada58fecfa22532341657138d3
Magdalena Przybylak <34132@student.ue.poznan.pl>
2016-03-23 13:20:44 +0100

    scalone z gazezia i maprawiony konflikt

--- kalkulator.py
+++ kalkulator.py
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
-def dodawanie():
-	a = int(input("Podaj a"))
-	b = int(input("Podaj b"))
-	print(a+b)
 
-def get_info():
-		return "TO jest program kalkulator"
-print(get_info()		
-dodawanie()	
-=======
 def dodawanie(a,b):
 	return a+b
+def get_info():
+	return "TO jest program kalkulator"
+	
+
 try:
 	a = int(input("podaj pierwsza liczbe"))	
 	b = int(input("podaj pierwsza liczbe"))	
 	print(dodawanie(a,b))	
 except ValueError as ve:
-	print("wprowadzono bledne dane, koncze dzialanie..")
->>>>>>> zmiana_funkcji_dodawania
+	print("wprowadzono bledne dane, koncze dzialanie..")	
+	
+print(get_info())	
 input()
