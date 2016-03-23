111d6d071cd9767d771423396705db493c3d1a53
Magdalena Przybylak <34132@student.ue.poznan.pl>
2016-03-23 12:57:26 +0100

    dodalam funkcje get info

--- kalkulator.py
+++ kalkulator.py
@@ -2,4 +2,9 @@ def dodawanie():
 	a = int(input("Podaj a"))
 	b = int(input("Podaj b"))
 	print(a+b)
+
+def get_info():
+		return "TO jest program kalkulator"
+print(get_info())		
 dodawanie()	
+input())
